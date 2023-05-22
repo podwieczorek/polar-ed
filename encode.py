32716df3c1dbba4d8590676b7fd3090a9aef1136
podwieczorek <paulina.wieczorek555@gmail.com>
2023-05-22 09:57:57 +0200

    minor changes for 'readability'

--- encode.py
+++ encode.py
@@ -3,12 +3,10 @@ import math
 
 
 def encode(N, K, message):
-    n = int(math.log2(N))  # exponent
 
-    # usecols parameter specifies which column to use
+    # usecols parameter specifies which column to use 0: N=1024; 1: N=512 ... 7: N=8
     reliability_sequence = np.genfromtxt('reliability_sequence.csv', dtype=int, delimiter=';', usecols={6},
-                                         skip_header=1,
-                                         usemask=True).compressed()
+                                         skip_header=1, usemask=True).compressed()
 
     # extracting message bits indexes from reliability sequence
     message_indexes = reliability_sequence[K:]
@@ -17,17 +15,20 @@ def encode(N, K, message):
     # other indexes will be set to 0 (frozen bits)
     u = np.zeros(N, dtype=int)
     u[message_indexes] = message
+
     # here starts proper encoding; going from bottom of the tree to top, number of levels equals to log2(N) - 1
-    # starting by combining 1 bit with 1 bit, on next steps number of combined bits is multiplied by 2
-    combined_bits_number = 1
-    for tree_depth in range(n - 1, -1, -1):
-        # iterating over vector, size step of 2 sub-channels
-        for i in range(0, N, 2 * combined_bits_number):
-            first_subchannel = u[i:i + combined_bits_number]
-            second_subchannel = u[i + combined_bits_number:i + 2 * combined_bits_number]
+    # starting by combining 1 bit with 1 bit, on each level number of combined bits is multiplied by 2
+    sequence_len = 1
+    tree_depth = int(math.log2(N))
+    for x in range(tree_depth - 1, -1, -1):
+        for i in range(0, N, 2 * sequence_len):
+            first_subchannel = u[i:i + sequence_len]
+            second_subchannel = u[i + sequence_len:i + 2 * sequence_len]
+
             # XOR-ing first_subchannel with second_subchannel, then concatenating the result with second subchannel
             combined_sequence = np.concatenate([(first_subchannel + second_subchannel) % 2, second_subchannel])
-            u[i:i + 2 * combined_bits_number] = combined_sequence
-        combined_bits_number *= 2
+            u[i:i + 2 * sequence_len] = combined_sequence
+
+        sequence_len *= 2
 
     return u
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from encode import encode
 if __name__ == "__main__":
     N = 16  # codeword length
     K = 8  # message bits
+
     # generating random binary message of size K
     message = np.random.randint(low=0, high=2, size=K)
     encoded_message = encode(N, K, message)
+
     print(encoded_message)
